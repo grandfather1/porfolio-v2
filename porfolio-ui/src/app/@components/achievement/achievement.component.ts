d9a737ef83b039e1040a50c4d9c46dbc381203e4
Foromo Daniel Soromou <foromo_daniel.soromou@bell.ca>
2019-03-18 00:43:01 -0400

    New base version of porfolio

---
+++ porfolio-ui/src/app/@components/achievement/achievement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'dx-achievement',
+    templateUrl: './achievement.component.html',
+    styleUrls: ['./achievement.component.scss']
+})
+
+export class AchievementComponent {
+
+    constructor() {
+        
+    }
+}
--- porfolio-ui/src/app/app.module.ts
+++ porfolio-ui/src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent} from './@components/header/header.component';
+import { HomeComponent } from './@components/home/home.component';
+import { AboutComponent } from './@components/about/about.component';
+import { AchievementComponent } from './@components/achievement/achievement.component';
+import { FooterComponent } from './@components/footer/footer.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        AboutComponent,
+        AchievementComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        NgbModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
